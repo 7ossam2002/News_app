cfcbee3aac3e937c33ace679c4622810d7356008
Hossam Hassan <hossamhassan2543@gmail.com>
2024-11-22 18:03:31 +0200

    +splash screen +home screen +images manager +strings manager +colors manager +app style

---
+++ lib/config/theme/app_style.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:news_app/core/utils/colors_manager.dart';
+
+class AppStyle{
+  static TextStyle appBar=  GoogleFonts.aboreto(fontWeight: FontWeight.w700,fontSize: 24.sp,color: ColorsManager.white);
+}
---
+++ lib/core/utils/colors_manager.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/rendering.dart';
+
+class ColorsManager{
+  static const Color black=Color(0xFF000000);
+  static const Color white=Color(0xFFFFFFFF);
+}
