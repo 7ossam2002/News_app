e6b35cf205c70c44275e81e16a7a47f62a098d1b
Hossam Hassan <hossamhassan2543@gmail.com>
2024-11-22 21:10:48 +0200

    +switch between two bages +drawer

--- lib/config/theme/app_style.dart
+++ lib/config/theme/app_style.dart
@@ -6,4 +6,7 @@ import 'package:news_app/core/utils/colors_manager.dart';
 
 class AppStyle{
   static TextStyle appBar=  GoogleFonts.aboreto(fontWeight: FontWeight.w700,fontSize: 24.sp,color: ColorsManager.white);
+  static TextStyle drawerTitle=  GoogleFonts.aboreto(fontWeight: FontWeight.w700,fontSize: 35.sp,color: ColorsManager.white);
+  static TextStyle drawerItem=  GoogleFonts.aBeeZee(fontWeight: FontWeight.w700,fontSize: 22.sp,color: ColorsManager.black);
+  static TextStyle pickCategory=  GoogleFonts.aBeeZee(fontWeight: FontWeight.w700,fontSize: 25.sp,color: ColorsManager.black);
 }
--- lib/core/utils/strings_manager.dart
+++ lib/core/utils/strings_manager.dart
@@ -1,3 +1,4 @@
 class StringsManager{
   static const String appTitle="News App";
+  static const String drawerTitle="News App !";
 }
